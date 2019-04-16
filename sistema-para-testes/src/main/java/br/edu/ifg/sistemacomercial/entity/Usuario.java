05cc94aac60422c5f76230d3278710251829889e
Lucas Zacarias <lucas.haiaiel@gmail.com>
2019-04-16 10:08:48 -0300

    Adição de Pasta Teste

---
+++ sistema-para-testes/src/main/java/br/edu/ifg/sistemacomercial/entity/Usuario.java
@@ -0,0 +1,81 @@
+package br.edu.ifg.sistemacomercial.entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Usuario implements Serializable{
+
+    private Long id;
+    private String nome;
+    private String login;
+    private String senha;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 73 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Usuario other = (Usuario) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+}
